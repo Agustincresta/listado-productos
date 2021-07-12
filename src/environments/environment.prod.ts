a1553e5f6514180c6e695f7a877e633f961eecb6
agustin <agustincresta@gmail.com>
2021-07-12 10:50:36 -0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyDm3xrZttw0VBZTRfvufrjHRPWaCl_PFNk",
+  authDomain: "angular-firebase-870a6.firebaseapp.com",
+  databaseURL: "https://angular-firebase-870a6-default-rtdb.firebaseio.com",
+  projectId: "angular-firebase-870a6",
+  storageBucket: "angular-firebase-870a6.appspot.com",
+  messagingSenderId: "822185899412",
+  appId: "1:822185899412:web:8e315c6453a663ad6f8b81"
+}
 };
